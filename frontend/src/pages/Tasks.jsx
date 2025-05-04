378555c8ed91efd9a0f21f086bfc335d024af10e
arslaan07 <arslaanbanday5@gmail.com>
2025-05-04 18:27:43 +0530

    tasks page size updated

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -55,15 +55,15 @@ const Tasks = () => {
             {
                 !loading && <div className=" mt-6 px-14 py-3">
                 <div className=" pt-4 flex gap-6">
-                  <Link to='/dashboard' className=" relative text-3xl text-white bg-blue-600 flex justify-center 
+                  <Link to='/dashboard' className=" relative text-2xl text-white bg-blue-600 flex justify-center 
                   items-center rounded-full w-[60px] h-[60px] hover:bg-blue-700">
                     <span className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-                      <IoArrowBack size={40} />
+                      <IoArrowBack size={30} />
                     </span>
                   </Link>
                   <button
                     onClick={openModal}
-                    className="text-3xl text-white px-4 py-2 bg-blue-600 w-fit rounded-md hover:bg-blue-700 flex 
+                    className="text-2xl text-white px-4 py-2 bg-blue-600 w-fit rounded-md hover:bg-blue-700 flex 
                       justify-center items-center transition-all duration-300"
                   >
                     <span className="mr-2">
