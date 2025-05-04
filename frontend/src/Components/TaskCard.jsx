742d32d25b1c880f4185eb9acee1af8770908fec
arslaan07 <arslaanbanday5@gmail.com>
2025-05-04 18:34:51 +0530

    tasks page size updated

--- frontend/src/Components/TaskCard.jsx
+++ frontend/src/Components/TaskCard.jsx
@@ -42,13 +42,13 @@ const TaskCard = ({ title, description, status, taskId, createdAt, completedAt }
   return (
     <>
     {
-                deleteLoading && <div className="flex items-center justify-center min-w-[250px] h-[60vh]"><Loader /></div>
+                deleteLoading && <div className="flex items-center justify-center min-w-[200px] max-w-[250px] h-[60vh]"><Loader /></div>
             }
             {
                 !deleteLoading && <div className='min-w-[250px] min-h-[340px] border-2 border-blue-600 text-black flex flex-col justify-between py-6 px-3 rounded-md'>
                 <div>
                   <div className='flex justify-between items-center mb-4 gap-4'>
-                    <h1 className='text-3xl font-semibold'>{title}</h1>
+                    <h1 className='text-2xl font-semibold'>{title}</h1>
                     <span 
                       style={{
                         backgroundColor: status == 'pending' ? 'red' : 
